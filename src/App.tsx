1d6eecc64f9518839dc2f829697742b7883a4601
Sam Glendenning <samglendenning@duck.com>
2023-06-23 14:24:18 +0100

    Initial commit with basic questions

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { type UseQueryResult, useQuery } from "@tanstack/react-query";
+import axios, { type AxiosError } from "axios";
 
-function App() {
+type Question = {
+  category: string;
+  type: "boolean" | "multiple";
+  difficulty: "easy" | "medium" | "hard";
+  question: string;
+  correct_answer: string;
+  incorrect_answers: string[];
+};
+
+interface QuestionsResponse {
+  response_code: number;
+  results: Question[];
+}
+
+const useQuestions = (): UseQueryResult<QuestionsResponse, Error> =>
+  useQuery<QuestionsResponse, Error, QuestionsResponse, [string]>(
+    ["questions"],
+    () => {
+      return axios
+        .get("https://opentdb.com/api.php?amount=10")
+        .then((response) => response.data);
+    },
+    {}
+  );
+
+const App = () => {
+  const { data, isLoading } = useQuestions();
+
+  const [questions, setQuestions] = React.useState<Question[]>([]);
+
+  React.useEffect(() => {
+    if (!isLoading && data) {
+      setQuestions(data.results);
+    } else {
+      setQuestions([]);
+    }
+  }, [isLoading, data]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>Quiz</header>
+      {questions.length === 0 ? (
+        <p>Loading...</p>
+      ) : (
+        questions.map((question, i) => (
+          <div>
+            <h3>Question {i + 1}</h3>
+            <p>{question.question}</p>
+          </div>
+        ))
+      )}
     </div>
   );
-}
+};
 
 export default App;
